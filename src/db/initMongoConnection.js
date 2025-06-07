5fa98774e974f568fecf27833de7650169b1f92b
Andrii Semenenko <oxonomy14@gmail.com>
2025-06-08 00:05:57 +0700

    fix2

--- src/db/initMongoConnection.js
+++ src/db/initMongoConnection.js
@@ -15,6 +15,7 @@ export const initMongoConnection = async () => {
       console.log('Mongo connection successfully established!');
     } catch (e) {
       console.log('Error while setting up mongo connection', e);
-      throw e;
+   
+      process.exit(1);
     }
   };
--- src/db/models/contact.js
+++ src/db/models/contact.js
@@ -5,21 +5,24 @@ const contactsSchema = new Schema(
     name: {
       type: String,
       required: true,
-        },
-        phoneNumber: {  type: String,
-            required: true,
-        },
-        email: {  type: String,
-            },   
+    },
+    phoneNumber: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+    },   
     isFavourite: {
       type: Boolean,
-      
       default: false,
-        },
-        contactType: {type: String,
-            required: true,
-            enum: ['work', 'home', 'personal'],
-            default: 'personal'},
+    },
+    contactType: {
+      type: String,
+      required: true,
+      enum: ['work', 'home', 'personal'],
+      default: 'personal'
+    },
   },
   {
     timestamps: true,
