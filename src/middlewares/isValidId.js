d74794c53e2de12c0e1fa7f3d57e465347c2d59d
Andrii Semenenko <oxonomy14@gmail.com>
2025-06-20 23:19:07 +0700

    fix-200625

--- src/middlewares/isValidId.js
+++ src/middlewares/isValidId.js
@@ -1,11 +1,12 @@
 import { isValidObjectId } from 'mongoose';
 import createHttpError from 'http-errors';
 
-export const isValidId = (idName='id') => (req, res, next) => {
-  const { contactId } = req.params[idName];
-  if (!isValidObjectId(contactId)) {
-    throw createHttpError(400, 'Invalid MongoDB id');
+export const isValidId = (idName = 'id') => (req, res, next) => {
+  const id = req.params[idName];
+  if (!isValidObjectId(id)) {
+    throw createHttpError(400, `Invalid MongoDB id in param ${idName}`);
   }
 
   next();
 };
+
